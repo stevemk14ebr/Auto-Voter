1bcf701205e4586eec535803557825bef8185f5e
stevemk14ebr <stevemk14ebr@gmail.com>
2015-03-13 18:27:34 -0400

    Initial Commit

---
+++ AutoClick/Form1.Designer.vb
@@ -0,0 +1,53 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Browser = New System.Windows.Forms.WebBrowser()
+        Me.AutoClick = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Browser
+        '
+        resources.ApplyResources(Me.Browser, "Browser")
+        Me.Browser.Name = "Browser"
+        '
+        'AutoClick
+        '
+        Me.AutoClick.Enabled = True
+        Me.AutoClick.Interval = 1000
+        '
+        'Form1
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.Browser)
+        Me.Name = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Browser As System.Windows.Forms.WebBrowser
+    Friend WithEvents AutoClick As System.Windows.Forms.Timer
+
+End Class
---
+++ AutoClick/Form1.vb
@@ -0,0 +1,94 @@
+﻿Imports Microsoft.Win32
+Imports System.IO
+Imports System.Runtime.InteropServices
+
+
+
+
+Public Class Form1
+    Const FEATURE_DISABLE_NAVIGATION_SOUNDS As Integer = 21
+    Const SET_FEATURE_ON_THREAD As Integer = &H1
+    Const SET_FEATURE_ON_PROCESS As Integer = &H2
+    Const SET_FEATURE_IN_REGISTRY As Integer = &H4
+    Const SET_FEATURE_ON_THREAD_LOCALMACHINE As Integer = &H8
+    Const SET_FEATURE_ON_THREAD_INTRANET As Integer = &H10
+    Const SET_FEATURE_ON_THREAD_TRUSTED As Integer = &H20
+    Const SET_FEATURE_ON_THREAD_INTERNET As Integer = &H40
+    Const SET_FEATURE_ON_THREAD_RESTRICTED As Integer = &H80
+
+    <DllImport("urlmon.dll")> _
+    <PreserveSig> _
+    Private Shared Function CoInternetSetFeatureEnabled(FeatureEntry As Integer, <MarshalAs(UnmanagedType.U4)> dwFlags As Integer, fEnable As Boolean) As <MarshalAs(UnmanagedType.[Error])> Integer
+    End Function
+
+    Dim Votes As Integer = 0
+    Dim StartTime, EndTime As DateTime
+    Dim ElapsedTime As TimeSpan
+    Dim GoBack As Boolean = False
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        CoInternetSetFeatureEnabled(FEATURE_DISABLE_NAVIGATION_SOUNDS, SET_FEATURE_ON_PROCESS, True)
+        Browser.Navigate(New Uri("http://www.surveygizmo.com/s3/1844796/Week-8-Voting-MSA-Sports-Friday-Night-Spotlight-Game-of-the-Week"))
+        Browser.ScriptErrorsSuppressed = True
+        Me.Text = "Loading WebPage"
+        StartTime = Now
+    End Sub
+
+
+    Private Sub AutoClick_Tick(sender As Object, e As EventArgs) Handles AutoClick.Tick
+        EndTime = Now
+        Dim Elem As HtmlElement
+        Dim Doc As HtmlDocument
+        Doc = Browser.Document
+
+        'Make sure document is valid
+        If (Doc IsNot Nothing) Then
+            'Get the radio button for norwin
+            Elem = Doc.GetElementById("sgE-1844796-1-2-10038")
+            If (Elem IsNot Nothing) Then
+                Elem.InvokeMember("Click") 'click button
+                Me.Text = "Found Option"
+            End If
+
+            'Get the vote button
+            Dim Button As HtmlElement
+            Button = Doc.GetElementById("sg_SubmitButton")
+            If (Button IsNot Nothing) Then
+                Button.InvokeMember("Click") 'click vote
+                StartTime = Now
+                GoBack = True
+                Me.Text = "Voted"
+
+                Me.Text = "Voted For Norwin " + Votes.ToString() + " Times, Elapsed Time: " + ElapsedTime.TotalSeconds.ToString() + " seconds"
+            End If
+
+            'Hyperlinks are a little different they dont have an ID, so get all elements with tags
+            'the tag for the text return to poll is A
+
+            'Grab all elements with 'A' tag name
+            'Dim Elems As HtmlElementCollection
+            'Elems = Doc.GetElementsByTagName("A")
+            'For Each El As HtmlElement In Elems 'loop all the elements
+            '    If El.OuterText = "Return To Poll" Then   'check if the text is the link we want
+            '        Votes += 1
+            '        Me.Text = "Voted For Steel Valley " + Votes.ToString() + " Times, Elapsed Time: " + ElapsedTime.TotalSeconds.ToString() + " seconds"
+            '        El.InvokeMember("Click")   'click it
+            '    End If
+            'Next
+
+
+            ElapsedTime = EndTime.Subtract(StartTime)
+        End If
+    End Sub
+
+    Private Sub Browser_DocumentCompleted(sender As Object, e As WebBrowserDocumentCompletedEventArgs) Handles Browser.DocumentCompleted
+        'Callback for when browser loads page
+        If (GoBack = True) Then
+            Votes += 1
+            Browser.GoBack()
+            GoBack = False
+            Me.Text = "Reloaded Poll"
+            EndTime = Now
+        End If
+    End Sub
+End Class
